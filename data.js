fd33f20c67f2e9446d88896300af132211aee955
E-RKP <edobackup111@gmail.com>
2025-05-27 15:53:38 +0200

    version 2

---
+++ data.js
@@ -0,0 +1,37 @@
+let car = {
+brand: 'Toyota',
+model: 'x',
+year: 1999,
+color: 'blue'
+}
+
+let ncar = Object.values(car)
+for (i=0; i<ncar.length;i++){
+  console.log(ncar[i])
+}
+
+car.engine = 'diesel'
+console.log(car)
+console.log(Object.keys(car))
+console.log(Object.values(car))
+console.log(car.hasOwnProperty('model'))
+
+let student = {
+  nome: 'luca',
+  cognome: 'rossi',
+  age: 18,
+  skill: ['matematica', 'geografia', 'robotica']
+
+}
+
+function getFullName(oggetto){
+
+  let nomeCompleto = oggetto.nome+' '+ oggetto.cognome
+  return nomeCompleto;
+
+}
+
+console.log(getFullName(student))
+
+student.skill.push('inglese')
+console.log(student)
